c85770bece42a99e10a5789b9803c22711ccd2cd
The_ARN <90953591+rralireza@users.noreply.github.com>
2022-04-19 13:16:43 +0430

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -9,7 +9,7 @@ int main()
     std::cout << "Hello and welcome to our shop! \n";
     std::cout << "Please Select a number: \n [Samsung is 1] [Apple is 2] [Sony is 3]\n";
     std::cin >> number;
-    std::cout << "Please tell us how many device do you want?\n [Samsung Price is 750$] [Apple is 1500$] [Sony is 950$]\n";
+    std::cout << "Please tell us how many devices do you want?\n [Samsung Price is 750$] [Apple is 1500$] [Sony is 950$]\n";
     std::cin >> total;
     if (number == 1)
     {
